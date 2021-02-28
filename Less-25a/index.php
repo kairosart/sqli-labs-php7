7397026fd2964d0e932c5b7775471950273efa1c
Kairos <kairosart2007@gmail.com>
2021-02-28 21:24:44 +0100

    Added.

--- Less-25a/index.php
+++ Less-25a/index.php
@@ -2,27 +2,27 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Less-25a Trick with OR & AND Blind</title>
+<title>Less-25 Trick with OR & AND</title>
 </head>
 
 <body bgcolor="#000000">
-<div style=" margin-top:60px;color:#FFF; font-size:23px; text-align:center">Welcome&nbsp;&nbsp;&nbsp;<font color="#FF0000"> Dhakkan </font><br>
+<div style=" margin-top:70px;color:#FFF; font-size:40px; text-align:center">Welcome&nbsp;&nbsp;&nbsp;<font color="#FF0000"> Dhakkan </font><br>
 <font size="3" color="#FFFF00">
 
 
 <?php
 //including the Mysql connect parameters.
-include("../sql-connections/sql-connect.php");
+include("../sql-connections/sqli-connect.php");
 
 
-// take the variables
+// take the variables 
 if(isset($_GET['id']))
 {
-$id=$_GET['id'];
-//logging the connection parameters to a file for analysis.
-$fp=fopen('result.txt','a');
-fwrite($fp,'ID:'.$id."\n");
-fclose($fp);
+	$id=$_GET['id'];
+	//logging the connection parameters to a file for analysis.
+	$fp=fopen('result.txt','a');
+	fwrite($fp,'ID:'.$id."\n");
+	fclose($fp);
 
 	//fiddling with comments
 	$id= blacklist($id);
@@ -32,19 +32,18 @@ fclose($fp);
 	$hint=$id;
 
 // connectivity 
-$sql="SELECT * FROM users WHERE id=$id LIMIT 0,1";
-$result=mysql_query($sql);
-$row = mysql_fetch_array($result);
-
+    $sql="SELECT * FROM users WHERE id=$id LIMIT 0,1";
+	$result=mysqli_query($con1, $sql);
+	$row = mysqli_fetch_array($result, MYSQLI_BOTH);
 	if($row)
 	{
-  		echo "<font size='5' color= '#99FF00'>";	
-	  	echo 'Your Login name:'. $row['username'];
-		//echo 'YOU ARE IN ........';	  	
-		echo "<br>";
-	  	echo 'Your Password:' .$row['password'];
-	  	echo "</font>";
-  	}
+        echo "<font size='5' color= '#99FF00'>";	
+        echo 'Your Login name:'. $row['username'];
+      //echo 'YOU ARE IN ........';	  	
+      echo "<br>";
+        echo 'Your Password:' .$row['password'];
+		echo "</font>";
+	}
 	else 
 	{
 		echo '<font size="5" color="#FFFF00">';
@@ -53,13 +52,13 @@ $row = mysql_fetch_array($result);
 		//echo "You have an error in your SQL syntax";
 		echo "</br></font>";	
 		echo '<font color= "#0000ff" font size= 3>';	
-	
 	}
 }
-	else 
+else 
 { 
 	echo "Please input the ID as parameter with numeric value";
 }
+
 
 function blacklist($id)
 {
@@ -71,14 +70,14 @@ function blacklist($id)
 
 
 
-?>
 
+?>
 </font> </div></br></br></br><center>
-<img src="../images/Less-25a.jpg" />
+<img src="../images/Less-25.jpg" />
 </br>
 </br>
 </br>
-<img src="../images/Less-25a-1.jpg" />
+<img src="../images/Less-25-1.jpg" />
 </br>
 </br>
 <font size='4' color= "#33FFFF">
